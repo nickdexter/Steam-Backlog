0b3b415837de266327ccf8131cf578ff609e223d
Nick Dexter <nickdex1@hotmail.com>
2026-04-28 05:49:08 +1000

    fix(routes): library '/' only showing first game

--- backend/routes/library.js
+++ backend/routes/library.js
@@ -5,22 +5,22 @@ const errorResponse = require('../utils/error');
 
 router.get('/', async (req, res) => {
     const steam64id = Number(req.steam64id);
-    
-    if (!Number.isInteger(steam64id)){
+
+    if (!Number.isInteger(steam64id)) {
         response = errorResponse(400, "Invalid steam64id")
         return res.json(response);
     }
-    
+
     const result = await db.query(
         'SELECT * FROM user_library WHERE steam64id = $1',
         [req.steam64id]
     );
 
-    if(result.rowCount <=0){
+    if (result.rowCount <= 0) {
         response = errorResponse(404, "Library not found", "user does not exist");
         return res.json(response);
     }
-    return res.json(result.rows[0]);
+    return res.json(result.rows);
 });
 
 module.exports = router;
